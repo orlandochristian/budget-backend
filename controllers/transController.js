f05ca1d969ed7a6d53d1e69c320e7d98c0f678f6
christian orlando <christianorlando@pursuit.org>
2023-04-19 10:15:30 -0400

    hfkskss

--- controllers/transController.js
+++ controllers/transController.js
@@ -3,22 +3,23 @@ const transController = express.Router();
 
 
 const transArray = require("../models/transactions.js");  
+const { off } = require('../app.js');
 
 transController.get("/", (req, res) => {    
     res.send(transArray);    
 }); 
 
 
-transController.get("/:index", (req,res) => {
-    const { index } = req.params
-
-    // if (transArray.find((e)=> ))
-
-    if (transArray[index]) {
-        res.send(transArray[index])
-    } else {
+transController.get("/:id", (req,res) => {
+    const { id } = req.params
+ 
+     if (transArray.find(element=>   element.transctionId == id )) {
+        res.send(transArray.find((e)=> e.transctionId == id))
+     } else {
         res.redirect("/400")
-    }  
+     }
+
+   
 })
 
 
@@ -27,7 +28,7 @@ transController.post('/', (req, res) => {
     const transaction = {
         transctionId: Math.floor(Math.random() * 999999),
         item_name: req.body.item_name,
-        amount: req.body.amount,
+        amount: Number(req.body.amount),
         date: req.body.date,
         from: req.body.from,
         category: req.body.category,
@@ -37,25 +38,44 @@ transController.post('/', (req, res) => {
    
   })
 
-  transController.put('/:index', (req, res) => {
-    const { index } = req.params;
-    if(transArray[index]) {
-      transArray[index] = req.body ;
-      res.status(200).json({ status: 200, message: "resource updated" });
-    } else {
-      res.redirect("/404")
+  transController.put('/:id', (req, res) => {
+    const { id } = req.params;
+    let indexArr = transArray.map((e,i) => {
+        if (e.transctionId == id ) {
+            return i
+        }
+        return -1
+    })
+   
+    if (indexArr.find(e => e != -1)) {
+        transArray[indexArr.find(e => Number(e) != -1)] = req.body;
+        res.status(200).json({ status: 200, message: "resource deleted" });
+    }else{
+        res.redirect("/404")
     }
+
+   
   })
+    
 
 
-  transController.delete('/:index', (req, res) => {
-    const { index } = req.params;
-    if(transArray[index]) {
-      transArray.splice(index, 1);
-      res.status(200).json({ status: 200, message: "resource deleted" });
-    } else {
-      res.redirect("/404")
+  transController.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    let indexArr = transArray.map((e,i) => {
+        if (e.transctionId == id ) {
+            return i
+        }
+        return -1
+    })
+   
+    if (indexArr.find(e => e != -1)) {
+        transArray.splice(indexArr.find(e => Number(e) != -1), 1);
+        res.status(200).json({ status: 200, message: "resource deleted" });
+    }else{
+        res.redirect("/404")
     }
+
+   
   })
 
 module.exports = transController;
